fb1d5b5c18e3d2e4dbd3f44deb94b617d4886ca5
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-18 18:26:15 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,12 @@
 class Triangle
 
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
 
   class TriangleError < StandardError
-    
+
   end
 end
