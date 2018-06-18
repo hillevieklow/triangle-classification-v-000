7f375cd5967ea4bc4a663a1a1de7b037513b192c
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-18 18:28:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,13 @@ class Triangle
     @length_3 = length_3
   end
 
-  class TriangleError < StandardError
+  def kind
+    if any
+  end
 
+  class TriangleError < StandardError
+    def message
+      "The given lengths are not a valid triangle."
+    end
   end
 end
